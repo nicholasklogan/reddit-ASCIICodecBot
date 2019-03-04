6dd60dab72bbfacd5ba1ed32ff3974e155a0cfbf
Nik <nicholasklogan@gmail.com>
2019-03-03 19:05:26 -0500

    more pythonic change
    Also, the if statement can probably be shortened to if post, but I didn't want to change the functionality

--- asciicodecbot.py
+++ asciicodecbot.py
@@ -11,9 +11,7 @@ if not os.path.isfile("posts_replied_to.txt"):
     posts_replied_to = []
 else:
     with open("posts_replied_to.txt", "r") as f:
-        posts_replied_to = f.read()
-        posts_replied_to = posts_replied_to.split("\n")
-        posts_replied_to = list(filter(None, posts_replied_to))
+        posts_replied_to = [post for post in f.read.split('\n') if post is not None]
 
 cache = ""
 
